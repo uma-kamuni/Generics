806f72e9ad7de8cd8cd249d76ece40c693b475ba
uma-kamuni <kamuniumaramesh@gmail.com>
2021-12-21 23:48:36 -0800

    REFACTORED: Added all 3 methods into Single Generic Method

--- src/com/bridgelabz/FindMax.java
+++ src/com/bridgelabz/FindMax.java
@@ -2,12 +2,31 @@ package com.bridgelabz;
 
 public class FindMax {
 
+	public static <T extends Comparable<T>> T getMax(T num1, T num2, T num3) { // Refactor all 3 in one Generic Method
+		T max = num1;
+		if (num2.compareTo(max) > 0)
+			max = num2;
+		if (num3.compareTo(max) > 0)
+			max = num3;
+		showMaxValue(num1, num2, num3, max);
+		return max;
+	}
+
+	private static <T extends Comparable<T>> void showMaxValue(T num1, T num2, T num3, T max) {
+		System.out.println("the input1 is " + num1 + "\ninput2 is " + num2 + "\ninput3 is " + num3);
+		System.out.println("Maximum : " + max + "\n");
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Welcome To The Program For Finding Maximum Number Among Three");
-		Comparison compare = new Comparison();
-		compare.maximumInteger();
-        compare.maximumFloat();
-        compare.maximumString();
+		Integer intValue1 = 300, intValue2 = 900, intValue3 = 60;
+		Float floatValue1 = 9.3f, floatValue2 = 3.5f, floatValue3 = 8.7f;
+		String stringValue1 = "Mango", stringValue2 = "Orange", stringValue3 = "PineApple";
+
+		FindMax maximum = new FindMax();
+		maximum.getMax(intValue1, intValue2, intValue3);
+		maximum.getMax(floatValue1, floatValue2, floatValue3);
+		maximum.getMax(stringValue1, stringValue2, stringValue3);
+
 	}
 
 }
